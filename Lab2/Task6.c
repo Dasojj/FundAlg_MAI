c0fe498fdbeb9b4cc634788db49520fc74f6e143
Dasojj <44905166+Dasojj@users.noreply.github.com>
2023-01-14 19:24:30 +0300

    Update Task6.c

--- Lab2/Task6.c
+++ Lab2/Task6.c
@@ -12,13 +12,13 @@ int search_substring(const char* str, const char* substr) {
             }
         }
         if (!substr[j]) {
-            return 1;
+            return i;
         }
     }
     return 0;
 }
 
-void search_in_files(const char* substr, int file_count, ...) {
+int* search_in_files(const char* substr, int file_count, ...) {
     va_list files;
     va_start(files, file_count);
     char* buffer = NULL;
@@ -26,24 +26,47 @@ void search_in_files(const char* substr, int file_count, ...) {
     int i;
     for (i = 0; i < file_count; i++) {
         FILE* file = va_arg(files, FILE*);
-        printf("Searching in file %d\n", i + 1);
         int line_number = 1;
         while (getline(&buffer, &buffer_size, file) != -1) {
-            if (search_substring(buffer, substr)) {
-                printf("Found '%s' on line %d\n", substr, line_number);
+            int sym = search_substring(buffer, substr);
+            if (sym) {
+                int *result = (int*)malloc(3 * sizeof(int));
+                if (!result) {
+                    fprintf(stderr, "malloc() failed: insufficient memory!\n");
+                    return NULL;
+                }
+                result[0] = i + 1;
+                result[1] = line_number;
+                result[2] = sym;
+                return result;
             }
             line_number++;
+            free(buffer);
         }
         rewind(file);
     }
     va_end(files);
     free(buffer);
+    return NULL;
 }
 
 int main() {
     FILE* file1 = fopen("file1.txt", "r");
+    if((file1 = fopen("file1.txt", "r"))==NULL){
+        printf("File opening failed\n");
+        return -1;
+    }
     FILE* file2 = fopen("file2.txt", "r");
-    search_in_files("example", 2, file1, file2);
+    if((file2 = fopen("file2.txt", "r"))==NULL){
+        printf("File opening failed\n");
+        fclose(file1);
+        return -2;
+    }
+    int *res = search_in_files("example", 2, file1, file2);
+    if(res){
+        printf("Substring found in file %d in line %d in position %d\n", res[0], res[1], res[2]);
+        free(res);
+    }
     fclose(file1);
     fclose(file2);
     return 0;
