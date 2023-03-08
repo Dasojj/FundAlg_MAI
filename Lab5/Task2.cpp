5a12ef6cab40a40c8c9bcf121570847ae6e32658
Dasojj <44905166+Dasojj@users.noreply.github.com>
2023-03-08 20:55:51 +0300

    Task2 done

--- Lab5/Task2.cpp
+++ Lab5/Task2.cpp
@@ -6,46 +6,44 @@
 
 class allocator : public memory {
 private:
-    logger_builder builder;
-    logger* log;
+    logger* _log;
+    void log(std::string const & str, logger::severity severity) const {
+        if (_log == nullptr) return;
+        _log->log(str, severity);
+    }
 public:
-    allocator(){
-        builder.add_stream("console", logger::error).add_stream("task2log.txt", logger::information);
-        log = builder.build();
-    }
-    allocator(const allocator& obj) {
-        builder = obj.builder;
-    }
-    allocator& operator=(const allocator& obj){
-        builder = obj.builder;
-        return *this;
-    }
+    allocator(logger *log = nullptr){ _log = log; }
+    allocator(const allocator& obj) = delete;
+    allocator& operator=(const allocator& obj) = delete;
+
     void* allocate(size_t target_size) const override {
         void* ptr = nullptr;
         try { ptr = ::operator new(target_size); }
         catch(...){
-            log->log("Error allocating memory", logger::error);
+            log("Error allocating memory", logger::error);
+            throw;
         }
-        std::string memory_info;
-        std::ostringstream info_stream(memory_info);
-        info_stream << "Allocated memory with adress <" << ptr << ">";
-        log->log(memory_info, logger::information);
+        std::ostringstream info_stream;
+        info_stream << "Allocated memory with adress < " << ptr << " >";
+        log(info_stream.str(), logger::information);
         return ptr;
     }
+    
     void deallocate(void* target_to_dealloc) const override {
-        std::string memory_info;
-        std::ostringstream info_stream(memory_info);
-        info_stream << "Deallocated memory with adress < " << target_to_dealloc << " > and bytes collection < ";
+        std::ostringstream info_stream;
+        info_stream << "Deallocated memory with adress < " << target_to_dealloc <<" > and bytes collection < ";
         unsigned char* bytes = static_cast<unsigned char*>(target_to_dealloc);
-        while(bytes) info_stream << *(bytes++) << " ";
+        for(int i = 0; i < 255; i++) info_stream << std::hex << std::setfill('0') << std::setw(2) << (int)bytes[i] << " ";
         info_stream << ">";
         ::operator delete(target_to_dealloc);
-        log->log(memory_info, logger::information);
+        log(info_stream.str(), logger::information);
     }
 };
 
 int main() {
-    allocator alloc;
+    logger_builder builder;
+    builder.add_stream("console", logger::error).add_stream("allocator_log", logger::information);
+    allocator alloc(builder.build());
     int* int_ptr = static_cast<int*>(alloc.allocate(sizeof(int)));
     *int_ptr = 42;
     std::cout << "int: " << *int_ptr << std::endl;
